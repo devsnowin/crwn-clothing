6ca42e5b4ce87c46d01b41392c0e0a87bc50c7be
SNOWIN J <20103104@hit.edu.in>
2021-08-14 10:53:28 +0530

    some changes in signIn and signUp

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -32,7 +32,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         } catch (error) {
             console.log('Error creating user', error.message);
         }
-        
+
     }
 
     return userRef;
